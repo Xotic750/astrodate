837b2d2216753c1e91e3890e58816d3ec71846ae
Xotic750 <xotic750@gmail.com>
2013-09-27 17:53:07 +0200

    Update tests to reflect changes

--- test/create.js
+++ test/create.js
@@ -96,7 +96,7 @@
                 hour = getRandomInt(0, 24);
                 fullArray.push(year);
                 fullArray.push(month);
-                fullArray.push(getRandomInt(1, AstroDate.daysInMonth(year, month)));
+                fullArray.push(getRandomInt(1, new AstroDate([year, month]).daysInMonth()));
                 fullArray.push(hour);
                 if (hour === 24) {
                     fullArray.push(0);
@@ -245,7 +245,7 @@
                 }
 
                 month = padLeadingZero(getRandomInt(1, 12), 2);
-                day = padLeadingZero(getRandomInt(1, AstroDate.daysInMonth(parseInt(year, 10), parseInt(month, 10))), 2);
+                day = padLeadingZero(getRandomInt(1, new AstroDate([parseInt(year, 10), parseInt(month, 10)]).daysInMonth()), 2);
                 hour = padLeadingZero(getRandomInt(0, 24), 2);
                 if (hour === "24") {
                     minute = "00";
