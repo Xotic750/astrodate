e4d60f8c3d9027f787a396eab6d9f2132a42114a
Xotic750 <xotic750@gmail.com>
2013-12-09 18:00:38 +0100

    Reinstate code after toObjectString fix

--- scripts/whichAstroDate.js
+++ scripts/whichAstroDate.js
@@ -5,6 +5,6 @@
     if ('1' === process.env.ASTRODATE_RAW) {
         module.exports = require('../lib/astrodate');
     } else {
-        module.exports = require('../lib/astrodate');
+        module.exports = require('../lib/astrodate.min');
     }
 }());
