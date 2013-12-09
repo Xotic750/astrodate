ee1240b63a1ee1b2113f95d927afae79168e73b0
Xotic750 <xotic750@gmail.com>
2013-12-09 20:27:57 +0100

    Make changes for the newly released browserify to try and solve the latest issues.

--- scripts/whichTape.js
+++ scripts/whichTape.js
@@ -3,8 +3,6 @@
     'use strict';
 
     if ('1' === process.env.ASTRODATE_TAPE) {
-        module.exports = require('tap').test;
-    } else if ('2' === process.env.ASTRODATE_TAPE) {
         module.exports = require('tape');
     } else {
         module.exports = require('tape-compact');
